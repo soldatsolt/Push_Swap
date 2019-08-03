ba55e4adaeb1f6001019dfe79a0049d650306b7b
Kiana Mills <kmills@at-o1.21-school.ru>
2019-08-03 06:39:15 +0300

    WOW ITS WORKING WOW 1ST TRY WOW

--- main.c
+++ main.c
@@ -6,17 +6,28 @@ t_stack	*create_stack(void)
 
 	stack = (t_stack *)malloc(sizeof(t_stack));
 	stack->next = NULL;
+	stack->n = 0;
 	return (stack);
 }
 
-void    put_n_to_stack(t_stack **stack, int n)
+void    put_n_to_a(t_push *push, int n)
 {
 	t_stack   *tmp;
 
-	tmp = *stack;
-	(*stack) = create_stack();
+	tmp = push->a;
+	push->a = create_stack();
 	tmp->n = n;
-	tmp->next = (*stack);
+	tmp->next = push->a;
+}
+
+void    put_n_to_b(t_push *push, int n)
+{
+	t_stack   *tmp;
+
+	tmp = push->b;
+	push->b = create_stack();
+	tmp->n = n;
+	tmp->next = push->b;
 }
 
 t_push	*create_push(void)
@@ -29,6 +40,17 @@ t_push	*create_push(void)
 	push->start_a = push->a;
 	push->start_b = push->b;
 	return (push);
+}
+
+void	sa(t_push *push)
+{
+	int	tmp;
+
+	if (!(push->start_a->n) || !(push->start_a->next->n))
+		return ;
+	tmp = push->start_a->next->n;
+	push->start_a->next->n = push->start_a->n;
+	push->start_a->n = tmp;
 }
 
 void    free_all_stack(t_stack *start)
@@ -51,12 +73,34 @@ void	free_push(t_push *push)
 	free_all_stack(push->start_b);
 }
 
-int		main(void)
+void	print_stack(t_stack *stack)
+{
+	while (stack->next)
+	{
+		ft_putnbr(stack->n);
+		ft_putchar('\n');
+		stack = stack->next;
+	}
+}
+
+int		main(int argc, char **argv)
 {
 	t_push	*push;
+	int 	i;
 
+	i = 1;
 	push = create_push();
 
 	printf("Hi, it's Push_Swap\n");
+	if (argc >= 2)
+	{
+		while (i < argc)
+		{
+			put_n_to_a(push, ft_atoi(argv[i]));
+			i++;
+		}
+	}
+	sa(push);
+	print_stack(push->start_a);
 	return (0);
 }
