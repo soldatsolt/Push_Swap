29bc99fef54e36bf10c02756664854abd49afe36
Kiana Mills <kmills@oa-o3.21-school.ru>
2019-08-27 05:41:13 +0300

    Code here is outstanding wow. No

--- main_push_swap.c
+++ main_push_swap.c
@@ -1,40 +1,55 @@
 #include "push_swap.h"
 
+void	pb_n_times(t_push *push, int n)
+{
+	int	i;
 
+	i = 0;
+	while (i < n)
+	{
+		pb(push);
+		ft_putstr("pb\n");
+		i++;
+	}
+}
 
+int		kol_vo_elementov_v_stacke(t_stack *stack)
+{
+	int	n;
 
+	n = 0;
+	if (stack)
+	{
+		while (stack)
+		{
+			stack = stack->next;
+			n++;
+		}
+		return (n);
+	}
+	return (n);
+}
 
+void	pa_n_times(t_push *push, int n)
+{
+	int	i;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	i = 0;
+	while (i < n)
+	{
+		pa(push);
+		ft_putstr("pa\n");
+		i++;
+	}
+}
 
 void	algos1(t_push *push)
 {
 	t_stack	*tmp;
 	int		kol_vo_elementov;
+	int		i;
 
+	i = 0;
 	kol_vo_elementov = 0;
 	if (push->start_a)
 		tmp = push->start_a;
@@ -45,11 +60,48 @@ void	algos1(t_push *push)
 		tmp = tmp->next;
 		kol_vo_elementov++;
 	}
-	while (kol_vo_elementov > -100)
+	tmp = push->start_a;
+	while (tmp && tmp->next)
 	{
-		pb(push);
-		kol_vo_elementov -= 2;
+		if (push->start_a && push->start_a->next && push->start_a->n > push->start_a->next->n)
+		{
+			sa(push);
+			ft_putstr("sa\n");
+		}
+		while (tmp->next && tmp->n < tmp->next->n)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		if (tmp && tmp->next && (tmp->n > tmp->next->n))
+		{
+			pb_n_times(push, i);
+			i = 0;
+		}
 	}
+	pa_n_times(push, kol_vo_elementov_v_stacke(push->start_b));
+
+
+
+
+
+
+
+
+
+	// while (i < kol_vo_elementov)
+	// {
+	// 	krasivo_vivod_check(push);
+	// 	if (push->start_a->n > push->start_a->next->n)
+	// 	{
+	// 		sa(push);
+	// 	}
+	// 	ft_putstr("______________\n");
+	// 	krasivo_vivod_check(push);
+	// 	ra(push);
+	// 	i++;
+	// 	ft_putstr("______________\n");
+	// }
 	krasivo_vivod_check(push);
 
 }
@@ -71,16 +123,6 @@ int		main(int argc, char **argv)
 	}
 	check_for_duplicates(push);
 	algos1(push);
-
-
-
-
-
-
-
-
-
-
 
 	return (0);
 }
